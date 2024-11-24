1eb86143cbad9a7f1d661095fb02e6e575ee5bb9
hugoheml <hugo-hermelin17@orange.fr>
2024-11-24 01:28:47 +0100

    feat(http-server): add handler logic

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,11 +3,21 @@ import express, { Request, Response } from 'express';
 import * as todoListRoutes from './routes/todoList.routes';
 
 import { RunScripts } from './db';
+import { ZodError } from 'zod';
 
 const app = express();
 
 app.use(express.json());
 app.use('/todo-list', todoListRoutes.default);
+
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+	if (err instanceof ZodError) {
+		res.status(400).json({ code: 400, message: 'Bad Request', errors: err.errors });
+		return;
+	} else  {
+		res.status(500).json({ code: 500, message: 'Internal Server Error' });
+	}
+})
 
 app.use((req, res) => {
 	res.status(404).send({ code: 404, message: 'Not Found' });
