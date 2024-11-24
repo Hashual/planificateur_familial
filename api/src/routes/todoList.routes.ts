4ddd8b623cf46a586dd9cfa8b92caa770b45d564
hugoheml <hugo-hermelin17@orange.fr>
2024-11-24 02:09:35 +0100

    feat(todoList): enhance GET /:listId route to include tasks

--- api/src/routes/todoList.routes.ts
+++ api/src/routes/todoList.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { createTodoList, getAllTodoLists, getTodoListById } from '../models/todo/todoList';
 import { handler } from '../utils/handler';
 import { z } from 'zod';
+import { getTodoListTasks } from '../models/todo/todoListTask';
 
 const router = Router();
 
@@ -11,46 +12,37 @@ router.get('/', async (req, res) => {
     res.status(200).json({ code: 200, message: 'Success', data: todoList });
 });
 
-router.all('/:listId*', handler({
-    params: {
-        listId: z.coerce.number().int()
-    },
-    handler: async (req, res, next) => {
-        const { listId } = req.params;
-
-        const todoList = await getTodoListById(listId);
-        if (!todoList) {
-            return res.status(404).json({ code: 404, message: 'Not Found' });
-        } 
-        await next();
-    }
-}));
-
-router.get('/:listId', handler({
-    params: {
-        listId: z.coerce.number().int()
-    },
-    handler: async (req, res) => {
-		if (res.headersSent) { return; }
-
-        const { listId } = req.params;
-        const todoList = await getTodoListById(listId)!;
-
-        res.status(200).json({ code: 200, message: 'Success', data: todoList });
-    }
-}));
-
 router.post('/', handler({
 	body: z.object({
 		title: z.string()
 	}),
 	handler: async (req, res) => {
 		const { title } = req.body;
+
 		const todoListId = await createTodoList(title);
 		const todoList = await getTodoListById(todoListId)!;
 
 		res.status(200).json({ code: 200, message: 'Created', data: todoList });
 	}
 }))
+
+router.get('/:listId', handler({
+	params: z.object({
+		listId: z.coerce.number().int()
+	}),
+    handler: async (req, res) => {
+		const { listId } = req.params;
+		const todoList = await getTodoListById(listId);
+
+		if (!todoList) {
+			res.status(404).json({ code: 404, message: 'Not Found' });
+			return;
+		}
+
+		const tasks = await getTodoListTasks(listId);
+
+		res.status(200).json({ code: 200, message: 'Success', data: { ...todoList, tasks } });
+	}
+}));
 
 export default router;
