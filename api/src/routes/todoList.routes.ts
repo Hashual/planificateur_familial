ec637f06bdc48c2eabe6dd8e697264f9e584a4ac
hugoheml <hugo-hermelin17@orange.fr>
2024-11-24 01:41:40 +0100

    feat(todoList): add /:listId middleware

--- api/src/routes/todoList.routes.ts
+++ api/src/routes/todoList.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import { getAllTodoLists } from '../models/todo/todoList';
+import { getAllTodoLists, getTodoListById } from '../models/todo/todoList';
+import { handler } from '../utils/handler';
+import { z } from 'zod';
 
 const router = Router();
 
@@ -7,6 +9,22 @@ router.get('/', async (req, res) => {
 	const todoList = await getAllTodoLists();
 
 	res.status(200).json({ code: 200, message: 'Success', data: todoList });
-})
+});
+
+router.all('/:listId*', handler({
+    params: {
+        listId: z.coerce.number().int()
+    },
+    handler: async (req, res, next) => {
+        const { listId } = req.params;
+
+        const todoList = await getTodoListById(listId);
+        if (!todoList) {
+            return res.status(404).json({ code: 404, message: 'Not Found' });
+        } 
+        await next();
+    }
+}));
+
 
 export default router;
