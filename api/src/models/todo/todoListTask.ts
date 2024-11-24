19f9e63cab26b06c11765a4b804ce11cdb62950f
hugoheml <hugo-hermelin17@orange.fr>
2024-11-24 01:44:07 +0100

    feat(todoListTask): add getListTasks function

--- api/src/models/todo/todoListTask.ts
+++ api/src/models/todo/todoListTask.ts
@@ -25,6 +25,21 @@ export const deleteTodoListTask = async(id: number): Promise<void> => {
     await SqlQuery<QueryResult>("DELETE FROM todoListTask WHERE id = ?", [id]);
 }
 
+export const getListTasks = async(todoListId: number): Promise<TodoListTask[]> => {
+    const result : RowDataPacket[] = await SqlQuery<RowDataPacket[]>("SELECT * FROM todoListTask WHERE todoListId = ?", [todoListId]);
+    return result.map((row: RowDataPacket) => {
+        return {
+            id: row.id,
+            title: row.title,
+            dueDate: row.dueDate ? new Date(row.dueDate) : null,
+            createdAt: new Date(row.createdAt),
+            updatedAt: new Date(row.updatedAt),
+            todoListId: row.todoListId,
+            isComplete: row.isComplete
+        }
+    }) as TodoListTask[];
+}
+
 export const getTasksAmount = async(todoListId: number, isComplete: boolean): Promise<number> => {
 	const result : RowDataPacket[] = await SqlQuery<RowDataPacket[]>("SELECT COUNT(*) as amount FROM todoListTask WHERE todoListId = ? AND isComplete = ?", [todoListId, isComplete]);
 	return result[0].amount;
