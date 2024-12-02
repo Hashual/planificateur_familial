00e15e406be8cf497048a2cd0cc67fddceff604d
Florian Claux <florian.claux@etu.unilim.fr>
2024-12-02 20:27:37 +0100

    refacto(Colors): add color const

--- free-go/constants/Colors.ts
+++ free-go/constants/Colors.ts
@@ -8,12 +8,10 @@ const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#141C24',
+    background: '#F7FAFA',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    icon: '#E3E8F2',
 
     /* Buttons */
     primary: '#F5C754'
@@ -23,8 +21,6 @@ export const Colors = {
     background: '#151718',
     tint: tintColorDark,
     icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
 
     /* Buttons */
     primary: '#F5C754'
