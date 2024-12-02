97354ece74707176dd3a7d3848d0d681d754e527
Hashual <loizeau.dorian05@gmail.com>
2024-12-02 17:30:08 +0100

    remove : exendent imports

--- free-go/app/HomePage.tsx
+++ free-go/app/HomePage.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router';
-
-import Fridge from '../components/homePage/shared/Fridge';
-import DoorText from '../components/homePage/doorPage/DoorText';
-import Handle from '../components/homePage/doorPage/Handle';
-import HandleReflection from '../components/homePage/doorPage/HandleReflection';
-import DoorReflection from '../components/homePage/doorPage/DoorReflection';
+import { View, StyleSheet} from 'react-native';
 import DoorPage from './DoorPage';
 
 const HomePage: React.FC = () => {
-  const router = useRouter();
-
-  const navigateToOpenDoor = () => {
-    router.push('../OpenDoorPage'); // Redirige vers la page de porte ouverte
-  };
 
   return (
     <View style = {styles.container}>
