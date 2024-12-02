47309b5884b609e43793864806ed3443b6e5ba47
Hashual <loizeau.dorian05@gmail.com>
2024-12-02 13:53:25 +0100

    refacto : change path and add new files

--- free-go/app/DoorPage.tsx
+++ free-go/app/DoorPage.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router';
-
 import Fridge from '../components/homePage/shared/Fridge';
 import DoorText from '../components/homePage/doorPage/DoorText';
 import Handle from '../components/homePage/doorPage/Handle';
 import HandleReflection from '../components/homePage/doorPage/HandleReflection';
 import DoorReflection from '../components/homePage/doorPage/DoorReflection';
+import { useRouter } from 'expo-router';
 
-const HomePage: React.FC = () => {
+const DoorPage: React.FC = () => {
   const router = useRouter();
 
-  const navigateToOpenDoor = () => {
-    router.push('../OpenDoorPage'); // Redirige vers la page de porte ouverte
+  const handleClick = () => {
+    router.push('../OpenDoorPage');
   };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={navigateToOpenDoor}>
+      <TouchableOpacity onPress={handleClick}>
         <Fridge>
           <DoorText />
           <Handle>
@@ -34,9 +33,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFF',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
-export default HomePage;
+export default DoorPage;
