864ebb8620cc65c7615c23d0a600c6a742e14018
hugoheml <51268820+hugoheml@users.noreply.github.com>
2024-12-02 15:45:44 +0000

    refacto(shopping-list): extract button colors as a constant

--- free-go/constants/Colors.ts
+++ free-go/constants/Colors.ts
@@ -14,6 +14,9 @@ export const Colors = {
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+
+    /* Buttons */
+    primary: '#F5C754'
   },
   dark: {
     text: '#ECEDEE',
@@ -22,5 +25,8 @@ export const Colors = {
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+
+    /* Buttons */
+    primary: '#F5C754'
   },
 };
