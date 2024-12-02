8cbe109ebf568fd6267be88a6a67ecd715d54698
Hashual <loizeau.dorian05@gmail.com>
2024-12-02 16:56:56 +0100

    fix : change views to redifine styleSheets

--- free-go/app/index.tsx
+++ free-go/app/index.tsx
@@ -1,4 +1,3 @@
-import { Stack } from 'expo-router';
 import HomePage from './HomePage';
 
 export default function App() {
--- free-go/components/homePage/openDoorPage/insideDoor.tsx
+++ free-go/components/homePage/openDoorPage/insideDoor.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import DoorShelfs from './DoorShelf';
+import { ThemedButton } from '@/components/ThemedButton';
+import { useRouter } from 'expo-router';
 
 interface InsideDoorProps {
   children?: React.ReactNode; // Permet d'ajouter des enfants
 }
 
 const InsideDoor: React.FC<InsideDoorProps> = ({ children }) => {
+  const router = useRouter(); // Initialisation du router pour la navigation
+
   return (
     <View style={styles.insideContainer}>
       <View style={styles.inside}>
         <View style={styles.shelfContainer}>
           <DoorShelfs /> {/* Première étagère */}
+          <ThemedButton
+            title="To-Do"
+            onPress={() => router.push('/todolists')} // Redirige vers la To-Do Liste
+            type="primary"
+            lightColor="#F5C754"
+            darkColor="#F5C754"
+            style={styles.button}
+          />
           <DoorShelfs /> {/* Deuxième étagère */}
+          <ThemedButton
+            title="Liste des courses"
+            onPress={() => router.push('/shoppinglists')} // Redirige vers la Liste des Courses
+            type="primary"
+            lightColor="#F5C754"
+            darkColor="#F5C754"
+            style={styles.button}
+          />
           <DoorShelfs /> {/* Troisième étagère */}
           <DoorShelfs /> {/* Quatrième étagère */}
-          <DoorShelfs /> {/* Cinquième étagère */}
         </View>
         {children && <View style={styles.childrenContainer}>{children}</View>}
       </View>
@@ -36,12 +55,17 @@ const styles = StyleSheet.create({
     height: '95%', // Hauteur relative pour s'adapter à la porte
     backgroundColor: '#FFFFFF', // Couleur blanche
     borderRadius: 20, // Coins légèrement arrondis
-    transform: [{ skewX: '-0deg' }, { skewY: '-5deg' }], // Déformation horizontale et verticale
+    transform: [{ skewX: '-0deg' }, { skewY: '-0deg' }], // Déformation horizontale et verticale
   },
   shelfContainer: {
     flex: 1,
     justifyContent: 'space-between', // Espacement uniforme des étagères
     paddingVertical: 10,
+  },
+  button: {
+    marginVertical: 10, // Espacement entre les boutons
+    alignSelf: 'center',
+    width: '80%', // Largeur ajustée pour un bon alignement
   },
   childrenContainer: {
     position: 'absolute', // Permet de superposer les enfants sans affecter les étagères
