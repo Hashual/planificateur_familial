c2e0d05c2a1eff2407a3777987ebd0548279e12a
Hashual <145881009+Hashual@users.noreply.github.com>
2024-12-04 14:38:27 +0100

    add : swipe effect on DoorPage

--- free-go/app/DoorPage.tsx
+++ free-go/app/DoorPage.tsx
@@ -1,37 +1,55 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Fridge from '../components/homePage/shared/Fridge';
 import DoorText from '../components/homePage/doorPage/DoorText';
 import Handle from '../components/homePage/doorPage/Handle';
 import HandleReflection from '../components/homePage/doorPage/HandleReflection';
 import DoorReflection from '../components/homePage/doorPage/DoorReflection';
+import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
 import { useRouter } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const DoorPage: React.FC = () => {
   const router = useRouter();
 
-  const handleClick = () => {
-    router.push('../OpenDoorPage');
+  // Gestion de l'événement lorsque le balayage est ouvert
+  const handleSwipeOpen = (direction: string) => {
+    if (direction === 'left') {
+      router.push('/OpenDoorPage'); // Naviguer uniquement si le balayage est à gauche
+    }
   };
 
   return (
-      <TouchableOpacity onPress={handleClick} style={styles.container}>
+    <GestureHandlerRootView>
+      <Swipeable
+        onSwipeableOpen={() => handleSwipeOpen('left')} // Appelée après un balayage valide
+        containerStyle={styles.container2}
+        childrenContainerStyle={styles.container}
+      >
         <Fridge>
           <DoorText />
           <Handle>
             <HandleReflection />
           </Handle>
-          <DoorReflection/>
+          <DoorReflection />
         </Fridge>
-      </TouchableOpacity>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-      justifyContent: 'center',
-      alignItems: 'center',
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'FFF',
+    justifyContent: 'center', 
+    alignItems: 'center',
   },
+  container2: {
+    flex: 1,
+    width: '100%',
+  }
 });
 
 export default DoorPage;
