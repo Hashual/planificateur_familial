364d4a4544416ab4cb98d338db16a80d66e18055
Florian Claux <florian.claux@etu.unilim.fr>
2024-12-04 14:07:17 +0100

    feat: Create RootView.tsx

---
+++ free-go/components/RootView.tsx
@@ -0,0 +1,15 @@
+import { Colors } from "@/constants/Colors";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { ViewProps } from "react-native";
+import { SafeAreaView, SafeAreaViewProps } from "react-native-safe-area-context";
+
+export type RootViewProps = SafeAreaViewProps & {
+    color?: keyof typeof Colors["light"],
+    padding?: number
+};
+
+export function RootView({color, padding, style, ...rest}: RootViewProps) {
+    const colors = useThemeColor();
+
+    return <SafeAreaView style={[{backgroundColor: colors[color ?? "background"], padding: padding, flex: 1}, style]} {...rest} />;
+}
