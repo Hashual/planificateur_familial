7449d1fdb71bf35dd3838ef16ecd5f88c58e6ac3
hugoheml <51268820+hugoheml@users.noreply.github.com>
2024-12-17 15:56:41 +0000

    chore: remove unused import

--- free-go/app/index.tsx
+++ free-go/app/index.tsx
@@ -1,6 +1,5 @@
 import LoadFont from '@/utils/LoadFont';
 import HomePage from './homePage/HomePage';
-import { useEffect } from 'react';
 import CheckToken from '@/utils/api/auth/CheckToken';
 
 export default function App() {
