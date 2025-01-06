b1eaef536cb51f0b30dfa340e63768cd0f17a030
Ebayart <elibayart05@gmail.com>
2025-01-06 13:19:33 +0100

    feat(db): add sql scripts for calendar and event table

---
+++ api/src/db/scripts/007_event.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS event (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    descritpion TEXT,
+    startDate TIMESTAMP,
+    endDate TIMESTAMP,
+    isRecurrent BOOLEAN NOT NULL DEFAULT FALSE,
+    color VARCHAR(255) NOT NULL,
+)
