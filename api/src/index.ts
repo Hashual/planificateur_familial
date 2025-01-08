9ec662a0674349f9173ffa72828f03143c2d264a
hugoheml <51268820+hugoheml@users.noreply.github.com>
2025-01-08 16:14:25 +0000

    fix: some issues with api & notifications

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,7 +11,6 @@ import { ZodError } from 'zod';
 import session from 'express-session';
 import HttpError from './utils/exceptions/HttpError';
 import dotenv from 'dotenv';
-import { Load } from './services/notifications';
 
 dotenv.config();
 
@@ -45,8 +44,6 @@ app.use('/todo-list', todoListRoutes);
 app.use('/shopping-list', shoppingListRoutes);
 app.use('/users', UserRoutes);
 app.use('/notifications', NotificationRoutes);
-
-Load();
 
 app.use((err: Error, req: Request, res: Response, next: Function) => {
 	if (err instanceof ZodError) {
