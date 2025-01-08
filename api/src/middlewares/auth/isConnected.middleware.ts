a30f1e74066b3ecafb52ca37b661ce5ed13d6763
hugoheml <51268820+hugoheml@users.noreply.github.com>
2025-01-08 09:50:14 +0000

    chore(user): add a isConnected middleware

---
+++ api/src/middlewares/auth/isConnected.middleware.ts
@@ -0,0 +1,26 @@
+import { Request } from "express";
+import { StatusCodes } from "http-status-codes";
+import HttpError from "../../utils/exceptions/HttpError";
+import { getSessionByToken } from "../../models/sessions/sessions";
+import { getUserById, User } from "../../models/user/user";
+
+export const isConnectedMiddleware = async (req: Request) => {
+	const authorizationHeader = req.headers['authorization'];
+	if (!authorizationHeader) {
+		throw new HttpError(StatusCodes.UNAUTHORIZED, 'Unauthorized');
+	}
+
+	const [tokenType, token] = authorizationHeader.split(' ');
+	if (tokenType !== 'Bearer' || !token) {
+		throw new HttpError(StatusCodes.UNAUTHORIZED, 'Unauthorized');
+	}
+
+	const session = await getSessionByToken(token);
+	if (!session) {
+		throw new HttpError(StatusCodes.UNAUTHORIZED, 'Unauthorized');
+	}
+
+	const sessionUser = await getUserById(session.userId)!;
+
+	return Object.assign(req, { user: sessionUser as User });
+}
