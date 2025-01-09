bab49fd6dce4e30cc542f2ee162c1f16408fbfa1
Lucas Bocquet <159040687+lbocquet0@users.noreply.github.com>
2025-01-09 09:18:21 +0100

    feat(doc): first test with apidoc and swagger

--- api/src/routes/shoppingList.routes.ts
+++ api/src/routes/shoppingList.routes.ts
@@ -10,6 +10,73 @@ import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 
 const router = Router();
 
+/**
+ * @api {get} /shopping-list Get Shopping Lists
+ * @apiName GetShoppingLists
+ * @apiGroup ShoppingList
+ * @apiSuccess {Object[]} shoppingLists List of Shopping Lists.
+ * @apiSuccess {Number} shoppingLists.id Shopping List unique ID.
+ * @apiSuccess {String} shoppingLists.title Shopping List title.
+ * @apiSuccess {String} shoppingLists.createdAt Shopping List creation date.
+ * @apiSuccess {String} shoppingLists.updatedAt Shopping List last update date.
+ * @apiSuccess {Object[]} shoppingLists.articles List of Shopping List articles.
+ * @apiSuccess {Number} shoppingLists.articles.id Article unique ID.
+ * @apiSuccess {String} shoppingLists.articles.name Article name.
+ * @apiSuccess {Number} shoppingLists.articles.quantity Article quantity.
+ * @apiSuccess {String} shoppingLists.articles.unit Article unit.
+ * @apiSuccess {String} shoppingLists.articles.createdAt Article creation date.
+ * @apiSuccess {String} shoppingLists.articles.updatedAt Article last update date.
+ * @apiSuccess {String} shoppingLists.articles.shoppingListId Shopping List unique ID.
+ * @apiSuccess {String} shoppingLists.articles.categoryId Article category unique ID.
+ * @apiSuccess {String} shoppingLists.articles.userId Article user unique ID.
+ * @apiSuccess {String} shoppingLists.articles.checked Article checked status.
+ * @apiSuccess {String} shoppingLists.articles.checkedAt Article checked date.
+ * @apiSuccess {String} shoppingLists.articles.checkedBy Article checked by user unique ID.
+ * @apiSuccess {String} shoppingLists.articles.checkedByName Article checked by user name.
+ * @apiSuccess {String} shoppingLists.articles.checkedByAvatar Article checked by user avatar.
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *    {
+ *    "code": 200,
+ *    "message": "OK",
+ *    "data": [
+ *    {
+ *    "id": 1,
+ *    "title": "Liste de courses",
+ *    "createdAt": "2021-04-23T17:00:00.000Z",
+ *    "updatedAt": "2021-04-23T17:00:00.000
+ *    "articles": [
+ *    {
+ *    "id": 1,
+ *    "name": "Pain",
+ *    "quantity": 1,
+ *    "unit": "unité",
+ *    "createdAt": "2021-04-23T17:00:00.000
+ *    "updatedAt": "2021-04-23T17:00:00.000
+ *    "shoppingListId": 1,
+ *    "categoryId": 1,
+ *    "userId": 1,
+ *    "checked": false,
+ *    "checkedAt": null,
+ *    "checkedBy": null,
+ *    "checkedByName": null,
+ *    "checkedByAvatar": null
+ *    }
+ *    ]
+ *    }
+ *    ]
+ *    }
+ *
+ *    @apiErrorExample Error-Response:
+ *    HTTP/1.1 500 Internal Server Error
+ *    {
+ *    "code": 500,
+ *    "message": "Failed to create shopping list"
+ *    }
+ *
+ *
+ */
 router.get('/', async (req, res) => {
     const shoppingLists = await getAllShoppingLists();
 
@@ -35,6 +102,14 @@ router.post('/', handler({
     },
 }));
 
+/**
+ * @api {get} /shopping-list/:listId Get Shopping List
+ * @apiName GetShoppingList
+ * @apiGroup ShoppingList
+ * @apiParam {Number} listId Shopping List unique ID.
+ * @apiSuccess {Object} shoppingList Shopping List details.
+ * @apiError {404} NotFound Shopping List not found.
+ */
 router.get('/:listId', handler({
     params: z.object({
         listId: SHOPPING_LIST_ID_TYPE,
