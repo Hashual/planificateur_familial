66ca51d7a9c87c909e995d009e2466031d0eb21f
Ebayart <elibayart05@gmail.com>
2025-01-09 10:57:34 +0100

    feat(doc): add documentation for todo List api

--- api/src/routes/todoList.routes.ts
+++ api/src/routes/todoList.routes.ts
@@ -9,10 +9,10 @@ import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 
 const router = Router();
 
-/**
+/** API GET
  * @api {get} /todo-list Get Todo Lists
  * @apiName GetTodoLists
- * @apiGroup TodoList
+ * @apiGroup Todo List
  * @apiParam {none} none
  * @apiSuccess {Object[]} data List of todo lists
  * @apiSuccess {Number} data.id Todo list id
@@ -20,33 +20,155 @@ const router = Router();
  * @apiSuccess {Date} data.createdAt Todo list creation date
  * @apiSuccess {Date} data.updatedAt Todo list update date
  * @apiSuccessExample {json} Success
- *  * HTTP/1.1 200 OK
+ * HTTP/1.1 200 OK
  * {
  *  "code": 200,
  *  "message": "OK",
- *  "data": 
- * [{	
-*  		"id": 1,
-*  		"title": "Todo list 1",
-* 		"createdAt": "2025-01-07T18:51:44.000Z",
-* 		"updatedAt": "2025-01-07T18:51:44.000Z"
-* }]
+ *  "data": {[
+ * 		"id": 1,
+ *  	"title": "Todo list 1",
+ * 		"createdAt": "2025-01-07T18:51:44.000Z",
+ * 		"updatedAt": "2025-01-07T18:51:44.000Z"
+ * 		]}
  * }
  * 
  * @apiSuccessExample {json} Success	
  * HTTP/1.1 200 OK
  * {
- * "code": 200,
- * "message": "OK",
- * "data": []
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": []
  * }
  * 	 
  *@apiErrorExample {none} Error
  * {
  * 
  * }
- *  
 */
+
+/** API POST
+ * @api {post} /todo-list Create Todo List
+ * @apiName CreateTodoList
+ * @apiGroup Todo List
+ * @apiParam {String} title Todo list title
+ * @apiSuccess {Object} data Todo list
+ * @apiSuccess {Number} data.id Todo list id
+ * @apiSuccess {String} data.title Todo list title
+ * @apiSuccess {Date} data.createdAt Todo list creation date
+ * @apiSuccess {Date} data.updatedAt Todo list update date
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": {[
+ * 		"id": 1,
+ * 		"title": "Todo list 1",
+ * 		"createdAt": "2025-01-07T18:51:44.000Z",
+ * 		"updatedAt": "2025-01-07T18:51:44.000Z"
+ * 		]}
+ * }
+ * 
+ * @apiSuccessExample {json} Success	
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": []
+ * }
+ * 
+ * @apiErrorExample {none} Error
+ * {
+ * 
+ * }
+*/	
+
+/** API GET
+ * @api {get} /todo-list/:listId Get Todo List Tasks by Todo List Id
+ * @apiName GetTodoListById
+ * @apiGroup Todo List
+ * @apiParam {Number} listId Todo list id
+ * @apiSuccess {Object} data Todo list
+ * @apiSuccess {Number} data.id Todo list id
+ * @apiSuccess {String} data.title Todo list title
+ * @apiSuccess {Date} data.createdAt Todo list creation date
+ * @apiSuccess {Date} data.updatedAt Todo list update date
+ * @apiSuccess {Object[]} data.tasks List of tasks
+
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": {[
+ * 		"id": 1,
+ * 		"title": "Todo list 1",
+ * 		"createdAt": "2025-01-07T18:51:44.000Z",
+ * 		"updatedAt": "2025-01-07T18:51:44.000Z",
+ * 		"tasks": [{
+ * 				"todoListId": 1,
+ * 				"title": "Task 1",
+ * 				"dueDate": "2025-01-07T18:51:44.000Z",
+ * 				"completedDate": "2025-01-07T18:51:44.000Z",
+ * 				"createdAt": "2025-01-07T18:51:44.000Z",
+ * 				"updatedAt": "2025-01-07T18:51:44.000Z"
+ * 			}]
+ * 		]}
+ * }
+ * 
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": {[
+ * 		"id": 1,
+ * 		"title": "Todo list 1",
+ * 		"createdAt": "2025-01-07T18:51:44.000Z",
+ * 		"updatedAt": "2025-01-07T18:51:44.000Z",
+ * 		"tasks": [{
+ * 				"todoListId": 1,
+ * 				"title": "Task 1",
+ * 				"dueDate": null,
+ * 				"completedDate": null,
+ * 				"createdAt": "2025-01-07T18:51:44.000Z",
+ * 				"updatedAt": "2025-01-07T18:51:44.000Z"
+ * 			}]	
+ * 		]}
+ * }
+ * 
+ * @apiSuccessExample {json} Success	
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": []
+ * }
+ * 
+ * @apiErrorExample {none} Error
+ * {
+ * 
+ * }
+*/ 
+
+/**
+ * API DELETE
+ * @api {delete} /todo-list/:listId Delete Todo List by Todo List Id
+ * @apiName DeleteTodoList
+ * @apiGroup Todo List
+ * @apiParam {Number} listId Todo list id
+ * @apiSuccess {none} none
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK"
+ *  }
+ *@apiErrorExample {none} Error
+ * {
+ * 
+ * }
+ */
 
 router.get('/', async (req, res) => {
     const todoList = await getAllTodoLists();
