94fa31db27f8a5174b60eff63fd6565829e4ae17
Ebayart <elibayart05@gmail.com>
2025-01-10 10:01:22 +0100

    feat(api): make route to delete sessions in bdd

--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import { handler } from "../utils/handler";
 import { isConnectedMiddleware } from "../middlewares/auth/isConnected.middleware";
 import { StatusCodes } from "http-status-codes";
+import { deleteSessionById } from "../models/sessions/sessions";
 
 const router = Router();
 
@@ -11,12 +12,31 @@ router.get('/me', handler({
 		const { user } = req;
 
 		res.status(StatusCodes.OK).json({
-			code: 200,
+			code: StatusCodes.OK,
 			data: user
 		})
 	}
 }))
 
-// TODO: A /logout route that will delete the session from the database
+router.post('/logout', handler({
+	use: isConnectedMiddleware,
+	handler: async (req, res) => {
+		const { user } = req;
+
+		const result = await deleteSessionById(user); 
+
+		if (result) {
+			res.status(StatusCodes.OK).json({
+				code: StatusCodes.OK,
+				message: 'Successfully logged out'
+			});
+		} else {
+			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+				code: StatusCodes.INTERNAL_SERVER_ERROR,
+				message: 'Failed to log out'
+			});
+		}
+	}}
+));
 
 export default router;
