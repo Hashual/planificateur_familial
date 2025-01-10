8cb9715e32e287e0431f828c2434e20faf506ebd
Hashual <145881009+Hashual@users.noreply.github.com>
2025-01-10 13:56:38 +0100

    change backpage

--- free-go/app/index.tsx
+++ free-go/app/index.tsx
@@ -1,12 +1,17 @@
 import HomePage from './homePage/HomePage';
 import CheckToken from '@/utils/api/auth/CheckToken';
+import { IsLogin } from '@/utils/api/auth/IsLogin';
 import RegisterForPushNotifications from '@/utils/api/notifications/RegisterForPushNotifications';
+import Login from './auth/login';
 
-export default function App() {
+export default async function App() {
 
     CheckToken();
     RegisterForPushNotifications();
 
-  return <HomePage />;
-
+  if (await IsLogin()) {
+    return <HomePage />;
+  }else  {
+    return <Login />;
+  }
 }
