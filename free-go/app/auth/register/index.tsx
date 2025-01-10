c93beead3d3044b9af89c11bebc843ab84cb9142
Hashual <loizeau.dorian05@gmail.com>
2025-01-10 13:56:38 +0100

    change backpage

--- free-go/app/auth/register/index.tsx
+++ free-go/app/auth/register/index.tsx
@@ -1,6 +1,8 @@
 import RegisterForm from "@/components/auth/forms/RegisterForm";
+import { SetBackPage } from "@/utils/SetBackPage";
 
 export default function Register() {
+	SetBackPage("/auth/login");
 	return (
 		<RegisterForm />
 	)
