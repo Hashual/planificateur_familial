1e04c0f39727be47fb443c5a30db5782913875c8
hugoheml <51268820+hugoheml@users.noreply.github.com>
2025-01-10 13:07:42 +0000

    feat(app): now redirect to the login page if you are not connected

--- free-go/app/index.tsx
+++ free-go/app/index.tsx
@@ -3,15 +3,25 @@ import CheckToken from '@/utils/api/auth/CheckToken';
 import { IsLogin } from '@/utils/api/auth/IsLogin';
 import RegisterForPushNotifications from '@/utils/api/notifications/RegisterForPushNotifications';
 import Login from './auth/login';
+import WaitingScreen from '@/components/utilities/WaitingScreen';
+import { router } from 'expo-router';
 
-export default async function App() {
+export default function App() {
 
     CheckToken();
     RegisterForPushNotifications();
 
-  if (await IsLogin()) {
-    return <HomePage />;
-  }else  {
-    return <Login />;
-  }
+    // Need a timeout here. Else, w'll have an infinite waiting screen
+    setTimeout(() => {
+        IsLogin().then( (isLogin) => {
+            if (isLogin) {
+                console.log('isLogin');
+                router.replace('/homePage/DoorPage');
+            } else {
+                router.replace('/auth/login');
+            }
+        })
+    }, 500)
+
+    return <WaitingScreen />;
 }
