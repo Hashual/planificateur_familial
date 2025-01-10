c4d62ec97f8aaca323d26882f775d4f462baa24b
Ebayart <elibayart05@gmail.com>
2025-01-10 09:01:30 +0100

    feat(doc): publish all the documentation for freego api

--- api/src/routes/shoppingListArticles.routes.ts
+++ api/src/routes/shoppingListArticles.routes.ts
@@ -10,6 +10,221 @@ import {StatusCodes, ReasonPhrases} from 'http-status-codes';
 
 const router = Router();
 
+/** API POST
+ * @api {post} /api/shopping-lists/:listId/articles Create a new article
+ * @apiName CreateShoppingListArticle
+ * @apiGroup ShoppingListArticles
+ * @apiParam {String} listId The id of the shopping list
+ * @apiParam {String} title The title of the article
+ * @apiParam {Date} [dueDate] The due date of the article
+ * @apiParam {Number} quantity The quantity of the article
+ * @apiSuccess {Object} data The list of articles
+ * @apiSuccess {Number} data.id The id of the article
+ * @apisuccess {Number} data.shoppingListId The id of the shopping list
+ * @apiSuccess {String} data.title The title of the article
+ * @apiSuccess {Number} data.quantity The quantity of the article
+ * @apiSuccess {Date} data.dueDate The due date of the article
+ * @apiSuccess {Date} data.completedAt The completion date of the article
+ * @apiSuccess {Date} data.createdAt The creation date of the article
+ * @apiSuccess {Date} data.updatedAt The last update date of the article
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": [{
+ *      "id": 1,
+ *      "shoppingListId": 1,
+ *      "title": "Article 1",
+ *      "quantity": 1,
+ *      "dueDate": "2021-09-01T00:00:00.000Z",
+ *      "completedAt": null,
+ *      "createdAt": "2021-09-01T00:00:00.000Z",
+ *      "updatedAt": "2021-09-01T00:00:00.000Z"}]
+ * }
+ * @apiErrorExample {json} 
+ * HTTP/1.1 500 INTERNAL_SERVER_ERROR
+ * {
+ *  "code": 500,
+ *  "message": "Failed to create article"
+ * }
+ * @apiErrorExample {json} List not found
+ * {
+ * 
+ * } 
+*/
+
+/** API PUT
+ * @api {put} /api/shopping-lists/:listId/articles/:articleId Update an article
+ * @apiName UpdateShoppingListArticle
+ * @apiGroup ShoppingListArticles
+ * @apiParam {String} listId The id of the shopping list
+ * @apiParam {String} articleId The id of the article
+ * @apiParam {String} title The title of the article
+ * @apiParam {Date} [dueDate] The due date of the article
+ * @apiParam {Date} [completedAt] The completion date of the article
+ * @apiSuccess {Object} data The list of articles
+ * @apiSuccess {Number} data.id The id of the article
+ * @apisuccess {Number} data.shoppingListId The id of the shopping list
+ * @apiSuccess {String} data.title The title of the article
+ * @apiSuccess {Number} data.quantity The quantity of the article
+ * @apiSuccess {Date} data.dueDate The due date of the article
+ * @apiSuccess {Date} data.completedAt The completion date of the article
+ * @apiSuccess {Date} data.createdAt The creation date of the article
+ * @apiSuccess {Date} data.updatedAt The last update date of the article
+ * @apiSuccessExample {json} Success
+ * // If the shopping list only have one article
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": [{
+ *      "id": 1,
+ *      "title":"Shopping List 1",
+ *      "createdAt": "2021-09-01T00:00:00.000Z",
+ *      "updatedAt": "2021-09-01T00:00:00.000Z",
+ *      articles: [{    
+ *          "id": 1,
+ *          "shoppingListId": 1,
+ *          "title": "Article 1",
+ *          "quantity": 1,
+ *          "dueDate": "2021-09-01T00:00:00.000Z",
+ *          "completedAt": null,
+ *          "createdAt": "2021-09-01T00:00:00.000Z",
+ *          "updatedAt": "2021-09-01T00:00:00.000Z"}]
+ * }
+ * @apiSuccessExample {json} Success
+ * // If the shopping list contains multiple articles
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": [{
+ *      "id": 1,
+ *      "title":"Shopping List 1",
+ *      "createdAt": "2021-09-01T00:00:00.000Z",
+ *      "updatedAt": "2021-09-01T00:00:00.000Z",
+ *      articles: [{    
+ *          "id": 1,
+ *          "shoppingListId": 1,
+ *          "title": "Tomatoes",
+ *          "quantity": 4,
+ *          "dueDate": "2021-09-01T00:00:00.000Z",
+ *          "completedAt": null,
+ *          "createdAt": "2021-09-01T00:00:00.000Z",
+ *          "updatedAt": "2021-09-01T00:00:00.000Z"},
+ *          {
+ *          "id": 2,
+ *          "shoppingListId": 1,
+ *          "title": "Potatoes",
+ *          "quantity": 2,
+ *          "dueDate": "2021-09-01T00:00:00.000Z",
+ *          "completedAt": null,
+ *          "createdAt": "2021-09-01T00:00:00.000Z",
+ *          "updatedAt": "2021-09-01T00:00:00.000Z"},
+ *         {
+ *          "id": 3,
+ *          "shoppingListId": 1,
+ *          "title": "Carrots",
+ *          "quantity": 3,
+ *          "dueDate": "2021-09-01T00:00:00.000Z",
+ *          "completedAt": null,
+ *          "createdAt": "2021-09-01T00:00:00.000Z",
+ *          "updatedAt": "2021-09-01T00:00:00.000Z"}]}]
+ * } 
+ * @apiErrorExample {json}
+ * HTTP/1.1 500 INTERNAL_SERVER_ERROR
+ * {
+ *  "code": 500,
+ *  "message": "Failed to update article"
+ * }
+ * @apiErrorExample {json} Article not found
+ * {
+ *  "code": 404,
+ *  "message": "Article not found"
+ * }
+ * @apiErrorExample {json} 
+ * {
+ * 
+ * }
+ */
+
+/** API DELETE
+ * @api {delete} /api/shopping-lists/:listId/articles/:articleId Delete an article
+ * @apiName DeleteShoppingListArticle
+ * @apiGroup ShoppingListArticles
+ * @apiParam {Number} listId The id of the shopping list
+ * @apiParam {Number} articleId The id of the article
+ * 
+ * @apiSuccess {Object} data The shopping list
+ * @apiSuccess {Number} data.id The id of the shopping list
+ * @apiSuccess {String} data.title The title of the shopping list
+ * @apiSuccess {Date} data.createdAt The creation date of the shopping list
+ * @apiSuccess {Date} data.updatedAt The last update date of the shopping list
+ * @apiSuccess {Array} data.articles The list of articles
+ * @apiSuccess {Number} data.id The id of the article
+ * @apisuccess {Number} data.shoppingListId The id of the shopping list
+ * @apiSuccess {String} data.title The title of the article
+ * @apiSuccess {Number} data.quantity The quantity of the article
+ * @apiSuccess {Date} data.dueDate The due date of the article
+ * @apiSuccess {Date} data.completedAt The completion date of the article
+ * @apiSuccess {Date} data.createdAt The creation date of the article
+ * @apiSuccess {Date} data.updatedAt The last update date of the article
+ * @apiSuccessExample {json} Success
+ * // If the shopping list only have one article
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "Article deleted successfully",
+ *  "data": [{
+ *      "id": 1,
+ *      "title":"Shopping List 1",
+ *      "createdAt": "2021-09-01T00:00:00.000Z",
+ *      "updatedAt": "2021-09-01T00:00:00.000Z",
+ *      articles: []}]
+ * }
+ * @apiSuccessExample {json} Success
+ * // If the shopping list contains multiple articles
+ * HTTP/1.1 200 OK
+ * {
+ *  "code": 200,
+ *  "message": "OK",
+ *  "data": [{
+ *      "id": 1,
+ *      "title":"Shopping List 1",
+ *      "createdAt": "2021-09-01T00:00:00.000Z",
+ *      "updatedAt": "2021-09-01T00:00:00.000Z",
+ *      articles: [{    
+ *          "id": 1,
+ *          "shoppingListId": 1,
+ *          "title": "Tomatoes",
+ *          "quantity": 4,
+ *          "dueDate": "2021-09-01T00:00:00.000Z",
+ *          "completedAt": null,
+ *          "createdAt": "2021-09-01T00:00:00.000Z",
+ *          "updatedAt": "2021-09-01T00:00:00.000Z"},
+ *          {
+ *          "id": 2,
+ *          "shoppingListId": 1,
+ *          "title": "Potatoes",
+ *          "quantity": 2,
+ *          "dueDate": "2021-09-01T00:00:00.000Z",
+ *          "completedAt": null,
+ *          "createdAt": "2021-09-01T00:00:00.000Z",
+ *          "updatedAt": "2021-09-01T00:00:00.000Z"}]}]
+ * }
+ * @apiErrorExample {json}
+ * HTTP/1.1 404 NOT_FOUND
+ * {
+ *  "code": 404,
+ *  "message": "Article not found"
+ * }
+ * @apiErrorExample {json}
+ * {
+ * 
+ * }
+*/
+
 router.post('/:listId/articles', handler({
     use : shoppingListIdMiddleware,
     params: z.object({
