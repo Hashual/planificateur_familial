3a53f45f7032a0df043c6a2560bddc80ba45a0f0
Lucas Bocquet <159040687+lbocquet0@users.noreply.github.com>
2025-01-13 09:48:43 +0100

    feat(api): add update route for todoList

--- api/src/routes/todoList.routes.ts
+++ api/src/routes/todoList.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
-import { createTodoList, deleteTodoList, getAllTodoLists, getTodoListById } from '../models/todo/todoList';
+import {
+	createTodoList,
+	deleteTodoList,
+	getAllTodoLists,
+	getTodoListById,
+	updateTodoList
+} from '../models/todo/todoList';
 import { handler } from '../utils/handler';
 import { z } from 'zod';
 import { getTodoListTasks } from '../models/todo/todoListTask';
@@ -198,6 +204,65 @@ router.post('/', handler({
 		res.status(StatusCodes.OK).json({ code: StatusCodes.OK, message: ReasonPhrases.OK, data: todoList });
 	}
 }))
+
+/** API PUT
+ * @api {put} /todo-list/:listId Update Todo List by Todo List Id
+ * @apiName UpdateTodoList
+ * @apiGroup Todo List
+ * @apiParam {Number} listId Todo list id
+ * @apiBody {String} title Todo list title
+ * @apiSuccess {Object} data Todo list
+ * @apiSuccess {Number} data.id Todo list id
+ * @apiSuccess {String} data.title Todo list title
+ * @apiSuccess {Date} data.createdAt Todo list creation date
+ * @apiSuccess {Date} data.updatedAt Todo list update date
+ * @apiSuccessExample {json} Success
+ * HTTP/1.1 200 OK
+ * {
+ * "code": 200,
+ * "message": "OK",
+ * "data": {
+ * 		"id": 1,
+ * 		"title": "Todo list 1",
+ * 		"createdAt": "2025-01-07T18:51:44.000Z",
+ * 		"updatedAt": "2025-01-07T18:51:44.000Z"
+ * 		}
+ * 	}
+ *
+ * @apiErrorExample {none} Error
+ * {
+ *  "code": 400,
+ *  "message": "Bad Request"
+ *  }
+ *
+ *  @apiErrorExample {none} Error
+ *  {
+ *  "code": 404,
+ *  "message": "Not Found"
+ *  }
+ *
+ **/
+
+router.put('/:listId', handler({
+	params: z.object({
+		listId: TODO_LIST_ID_TYPE
+	}),
+	use: todoListIdMiddleware,
+	body: z.object({
+		title: z.string()
+	}),
+	handler: async (req, res) => {
+		const { todoList } = req;
+		const { title } = req.body;
+
+		await updateTodoList(todoList.id, title);
+
+		const updatedTodoList = await getTodoListById(todoList.id);
+
+		res.status(StatusCodes.OK).json({ code: StatusCodes.OK, message: ReasonPhrases.OK, data: updatedTodoList });
+	}
+}))
+
 
 router.get('/:listId', handler({
 	params: z.object({
