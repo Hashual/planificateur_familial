a8bcfc135353603e2a0ae08ef320aa9dd400d313
Hashual <loizeau.dorian05@gmail.com>
2025-01-13 14:34:18 +0100

    Change display of UserInfo

--- free-go/components/user/UserInfos.tsx
+++ free-go/components/user/UserInfos.tsx
@@ -1,6 +1,8 @@
 import { GetUserInfosFromCache } from "@/utils/api/auth/UserInfos";
 import React, { useEffect } from "react";
 import { Image, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { ThemedText } from "../utilities/ThemedText";
 
 export function MeUserInfos() {
 	const [userInfos, setUserInfos] = React.useState(<></>)
@@ -28,12 +30,30 @@ export default function UserInfos({ avatarUrl, displayName }: UserInfosProps) {
 	// TODO: Improve the design
 	// Don't forget that the avatarUrl can be null (put a default image)
 	return (
-		<>
+		<View style={styles.container}>
 			<Image 
 				style={{ width: 50, height: 50 }}
 				source={{ uri: avatarUrl }}
 			/>
-			<Text>{displayName}</Text>
-		</>
+			<View style={styles.text}>
+				<ThemedText>{displayName}</ThemedText>
+			</View>
+		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		paddingBottom: 5,
+		paddingHorizontal: 5,
+		flexDirection: 'row',
+		width: '100%',
+		height: '100%',
+		flex: 1,
+	},
+	text:{
+		width: '100%',
+		height: '100%',
+		marginLeft: 10,
+	}
+})
