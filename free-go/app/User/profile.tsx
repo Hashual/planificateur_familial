884ee48a5a7b779b83f87b62ef0fdcf2d71bb135
Hashual <loizeau.dorian05@gmail.com>
2025-01-20 11:18:26 +0100

    start to create the profile View

--- free-go/app/User/profile.tsx
+++ free-go/app/User/profile.tsx
@@ -1,9 +1,26 @@
+import { MeUserPicture } from "@/components/user/UserPicture";
+import { MeUserName } from "@/components/user/UserName";
+import { ThemedText } from "@/components/utilities/ThemedText";
+import React from "react";
+import { StyleSheet,View } from "react-native";
+
 const Profile = () => {
     return (
-        <div>
-            <h1>Profile</h1>
-        </div>
+        <View style={styles.container}>
+            <ThemedText variant="mainTitle">Profile</ThemedText>
+            <MeUserPicture/>
+            <MeUserName/>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container :{
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        flexDirection: 'column',
+    }
+});
 
 export default Profile;
